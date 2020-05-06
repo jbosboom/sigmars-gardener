a8c134aca253f335f546c905835fe8f614c47c59
Jeffrey Bosboom <jeffreybosboom@gmail.com>
2020-05-06 11:42:48 -0700

    GFTT and MSER histograms
    These give decent signal.
    
    One problem we're having is that the chi-squared against an empty
    histogram (when we don't have any circles or keypoints) is 0, even
    though empty versus nonempty should be a relatively strong signal.

--- train.py
+++ train.py
@@ -1,12 +1,42 @@
 #!/usr/bin/env
 
 import sys, os
+import math
+from math import atan2, sqrt
 import cv2 as cv
 import numpy as np
+
+def compute_keypoint_histogram(image, detector):
+    kps = detector.detect(image)
+    polar_kps = []
+    for kp in kps:
+        x, y = kp.pt
+        dist = sqrt((x - 31) ** 2 + (y - 31) ** 2)
+        if dist >= 20: continue
+        phi = atan2(y, x)
+        polar_kps.append([(dist, phi)])
+    if polar_kps:
+        polar_kps = np.array(polar_kps, dtype=np.float32)
+        kp_hist = cv.calcHist([polar_kps], [0, 1], None, [20, 16], [0, 20, -math.pi, math.pi])
+        cv.normalize(kp_hist, kp_hist, alpha=0.0, beta=1.0, norm_type=cv.NORM_MINMAX)
+    else:
+        kp_hist = np.zeros((20, 16), dtype=np.float32)
+    return kp_hist
+
+def avg_chi(histograms):
+    cross_chi = [cv.compareHist(h, j, cv.HISTCMP_CHISQR) for h in histograms for j in histograms if
+        id(h) != id(j)]
+    if len(cross_chi):
+        avg_chi = sum(cross_chi) / len(cross_chi)
+    else:
+        avg_chi = -1.0
+    return avg_chi
 
 def compute_histograms(class_name, instance_files):
     color_histograms = []
     circle_histograms = [] # histogram of circle sizes in the image
+    gftt_histograms = []
+    mser_histograms = []
     for f in instance_files:
         image = cv.imread(f)
         color_hist = cv.calcHist([image], [0, 1, 2], None, [64, 64, 64], [0, 256, 0, 256, 0, 256])
@@ -30,25 +60,22 @@ def compute_histograms(class_name, instance_files):
             circle_hist = np.zeros((13, 1), dtype=np.float32)
         circle_histograms.append(circle_hist)
 
-    color_cross_chi = [cv.compareHist(h, j, cv.HISTCMP_CHISQR) for h in color_histograms for j in color_histograms if id(h) != id(j)]
-    if len(color_cross_chi):
-        color_avg_chi = sum(color_cross_chi)/len(color_cross_chi)
-    else:
-        color_avg_chi = -1.0
+        gftt_histograms.append(compute_keypoint_histogram(image, cv.GFTTDetector.create(maxCorners=10000)))
+        mser_histograms.append(compute_keypoint_histogram(image, cv.MSER.create()))
 
-    circle_cross_chi = [cv.compareHist(h, j, cv.HISTCMP_CHISQR) for h in circle_histograms for j in circle_histograms if id(h) != id(j)]
-    if len(circle_cross_chi):
-        circle_avg_chi = sum(circle_cross_chi)/len(circle_cross_chi)
-    else:
-        circle_avg_chi = -1.0
-
-    print('{:20} {:6.2f} {:6.2f}'.format(class_name, color_avg_chi, circle_avg_chi))
+    color_avg_chi = avg_chi(color_histograms)
+    circle_avg_chi = avg_chi(circle_histograms)
+    gftt_avg_chi = avg_chi(gftt_histograms)
+    mser_avg_chi = avg_chi(mser_histograms)
+    print('{:20} {:6.2f} {:6.2f} {:6.2f} {:6.2f}'.format(class_name, color_avg_chi, circle_avg_chi, gftt_avg_chi, mser_avg_chi))
 
     # We average the histograms without renormalizing them, particularly so that
     # an all-0 circle histogram is weighted against histograms with some 1s.
     color_hist = sum(color_histograms)/len(color_histograms)
     circle_hist = sum(circle_histograms)/len(circle_histograms)
-    return color_hist, circle_hist
+    gftt_hist = sum(gftt_histograms)/len(gftt_histograms)
+    mser_hist = sum(mser_histograms)/len(mser_histograms)
+    return color_hist, circle_hist, gftt_hist, mser_hist
 
 def main(args):
     class_dirs = os.listdir('data/classes/')
