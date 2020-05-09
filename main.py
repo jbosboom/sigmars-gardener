3446f3790df5712a42ac6756f3b5d9be4561bbf4
Jeffrey Bosboom <jeffreybosboom@gmail.com>
2020-05-08 18:24:29 -0700

    Move the mouse; detect mistakes

--- main.py
+++ main.py
@@ -26,6 +26,17 @@ def extract_and_clean(screenshot, region_center):
     pixel_data_hash = digester.hexdigest()
     return region, pixel_data_hash
 
+def parse_screenshot(screenshot, classifier):
+    puzzle = {}
+    for i, region_center in enumerate(data.centers):
+        image, pixel_data_hash = extract_and_clean(screenshot, region_center)
+        thing = classifier(image, pixel_data_hash)
+        if thing.endswith('-inactive'):
+            thing = thing[:-9]
+        if thing != 'blank':
+            puzzle[i] = thing
+    return puzzle
+
 def main(args):
     classifier_hash = hashlib.sha1()
     class_dirs = [x for x in Path('data/classes').iterdir()]
@@ -50,14 +61,7 @@ def main(args):
 
     upper_left_x, upper_left_y, _, _ = pyautogui.locateOnScreen('data/upper-left.png')
     screenshot = pyautogui.screenshot(region=(upper_left_x, upper_left_y, 1920, 1080))
-    puzzle = {}
-    for i, region_center in enumerate(data.centers):
-        image, pixel_data_hash = extract_and_clean(screenshot, region_center)
-        thing = classifier(image, pixel_data_hash)
-        if thing.endswith('-inactive'):
-            thing = thing[:-9]
-        if thing != 'blank':
-            puzzle[i] = thing
+    puzzle = parse_screenshot(screenshot, classifier)
     print(puzzle)
 
     histogram = {v: 0 for v in puzzle.values()}
@@ -73,6 +77,31 @@ def main(args):
 
     moves = solver.solve(puzzle)
     print(moves)
+
+    while moves:
+        a, b = moves.pop(0) # could reverse the list, I guess
+        cx, cy = data.centers[a]
+        pyautogui.click(upper_left_x + cx, upper_left_y + cy)
+        cx, cy = data.centers[b]
+        pyautogui.click(upper_left_x + cx, upper_left_y + cy)
+
+        # Check if our identification of any puzzle elements changed.  If so,
+        # write out the old screenshot and re-solve with the new information.
+        # We might fail to re-solve if we've used up elements we need.
+        mistaken = False
+        new_screenshot = pyautogui.screenshot(region=(upper_left_x, upper_left_y, 1920, 1080))
+        new_puzzle = parse_screenshot(new_screenshot, classifier)
+        for k, v in new_puzzle.items():
+            if puzzle[k] != v:
+                mistake_dir = xdg.XDG_RUNTIME_DIR / 'sigmars-gardener'
+                mistake_dir.mkdir(exist_ok=True)
+                mistake_image, mistake_hash = extract_and_clean(new_screenshot, data.centers[k])
+                mistake_image.save(mistake_dir / (mistake_hash + '.png'))
+                mistaken = True
+        if mistaken:
+            moves = solver.solve(new_puzzle)
+            if not moves:
+                raise Exception("mistake made puzzle unsolvable")
 
 
 if __name__ == '__main__':
